f18f1f12db972e97bd0d207d2e9c69077d48c710
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-04-22 09:28:57 -0300

    chore: setup Project

---
+++ rexe/ex/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Field from './field';
+
+ReactDOM.render(
+   <Field initialValue='test' />
+    ,document.getElementById('app')
+  );
+  
